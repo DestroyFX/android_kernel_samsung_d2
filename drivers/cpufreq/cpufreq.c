a436cec3f13dfee42c14680374ba3734b1d4e865
rogersb11 <brettrogers11@gmail.com>
2015-02-16 11:34:02 -0500

    Voltage interface for GPU

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -31,6 +31,8 @@
 #include <linux/syscore_ops.h>
 
 #include <trace/events/power.h>
+extern ssize_t get_gpu_vdd_levels_str(char *buf);
+extern void set_gpu_vdd_levels(int uv_tbl[]);
 
 /**
  * The "cpufreq driver" - the arch- or hardware-dependent low
@@ -613,6 +615,21 @@ static ssize_t show_scaling_setspeed(struct cpufreq_policy *policy, char *buf)
 	return policy->governor->show_setspeed(policy, buf);
 }
 
+ssize_t show_GPU_mV_table(struct cpufreq_policy *policy, char *buf)
+{
+		int modu = 0;
+		return get_gpu_vdd_levels_str(buf);
+}
+
+ssize_t store_GPU_mV_table(struct cpufreq_policy *policy, const char *buf, size_t count)
+{
+		unsigned int ret = -EINVAL;
+		unsigned int u[3];
+		ret = sscanf(buf, "%d %d %d", &u[0], &u[1], &u[2]);
+		set_gpu_vdd_levels(u);
+		return count;
+}
+
 /**
  * show_scaling_driver - show the current cpufreq HW/BIOS limitation
  */
@@ -658,6 +675,7 @@ cpufreq_freq_attr_rw(scaling_max_freq);
 cpufreq_freq_attr_rw(scaling_governor);
 cpufreq_freq_attr_rw(scaling_setspeed);
 cpufreq_freq_attr_rw(UV_mV_table);
+cpufreq_freq_attr_rw(GPU_mV_table);
 
 static struct attribute *default_attrs[] = {
 	&cpuinfo_min_freq.attr,
@@ -673,6 +691,7 @@ static struct attribute *default_attrs[] = {
 	&scaling_available_governors.attr,
 	&scaling_setspeed.attr,
 	&UV_mV_table.attr,
+	&GPU_mV_table.attr,
 	NULL
 };
 
